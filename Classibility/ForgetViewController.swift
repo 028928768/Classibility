b50015a106fafad8709be9a59bb2f3f91ef32a01
Kanta'MacPro <mfireball@kantamacpros-macbook-pro.local>
2018-10-26 00:34:22 +0700

    authentication system complete

--- Classibility/ForgetViewController.swift
+++ Classibility/ForgetViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import Firebase
 
 class ForgetViewController: UIViewController {
     //MARK Images
     let forgetImage = UIImage(named: "ForgetIMG")
     //MARK Outlets
     @IBOutlet weak var forgetPanelOutlet: UIImageView!
+    @IBOutlet weak var ForgetEmail: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +25,22 @@ class ForgetViewController: UIViewController {
         forgetPanelOutlet.image = forgetImage
     }
 
+    @IBAction func resendMethod(_ sender: Any) {
+        Auth.auth().sendPasswordReset(withEmail: ForgetEmail.text!) { (error) in
+            if error == nil{
+                self.performSegue(withIdentifier: "forgetToResend", sender: self)
+            }
+            else{
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+        
+        
+    }
     /*
     // MARK: - Navigation
 
--- Classibility/RegisterViewController.swift
+++ Classibility/RegisterViewController.swift
@@ -44,12 +44,24 @@ class RegisterViewController: UIViewController {
     }
     
     @IBAction func signupMethod(_ sender: Any) {
-        Auth.auth().createUser(withEmail: registerEmailText.text!, password: registerPasswordText.text!) { (user, error) in
-            if error == nil {
-                self.performSegue(withIdentifier: "signInSuccess", sender: self)
+            Auth.auth().createUser(withEmail: registerEmailText.text!, password: registerPasswordText.text!){ (user, error) in
+                if error == nil {
+                    self.performSegue(withIdentifier: "signInSuccess", sender: self)
+                }
+                else{
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
             }
-            
-        }
+        
+        
+        
+        
+        
+        
     }
     
     /*
