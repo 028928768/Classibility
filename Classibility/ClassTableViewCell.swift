06a7ba28a12816143f0bdfe4a5ea7da99294283e
Kanta'MacPro <mfireball@kantamacpros-macbook-pro.local>
2018-11-03 22:38:58 +0700

    Home page

---
+++ Classibility/ClassTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClassTableViewCell.swift
+//  Classibility
+//
+//  Created by Kanta'MacPro on 3/11/2561 BE.
+//  Copyright © 2561 Kanta'MacPro. All rights reserved.
+//
+
+import UIKit
+
+class ClassTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
